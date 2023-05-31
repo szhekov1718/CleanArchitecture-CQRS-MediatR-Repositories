0b081b04f0427d6cb3643d9b1dc46a1c3857a071
Stanimir Zhekov <s.zhekov1718@gmail.com>
2023-05-31 17:49:29 +0300

    Add Email Sender Service

--- HRLeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ HRLeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HRLeaveManagement.Application.Contracts.Email;
+using HRLeaveManagement.Application.Models.Email;
+using HRLeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HRLeaveManagement.Infrastructure
 {
     public static class InfrastructureServiceRegistration
     {
-        public static IServiceCollection ConfigureInfrastructureServices (this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+
             return services;
         }
     }
