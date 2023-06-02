d63aebec23d62b60fe1166511619da0ccbc99cf7
Stanimir Zhekov <s.zhekov1718@gmail.com>
2023-06-02 17:25:43 +0300

    Add Exception handling Middleware

---
+++ HRLeaveManagement.Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,78 @@
+﻿using HRLeaveManagement.Api.Models;
+using HRLeaveManagement.Application.Exceptions;
+using System.Net;
+
+namespace HRLeaveManagement.Api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            CustomProblemDetails problem = new();
+
+            switch (ex)
+            {
+                case Application.Exceptions.BadRequestException badRequestException:
+
+                    statusCode = HttpStatusCode.BadRequest;
+
+                    problem = new CustomProblemDetails
+                    {
+                        Title = badRequestException.Message,
+                        Status = (int)statusCode,
+                        Detail = badRequestException.InnerException?.Message,
+                        Type = nameof(Application.Exceptions.BadRequestException),
+                        Errors = badRequestException.ValidationErrors
+                    };
+                    break;
+
+                case EntityNotFoundException notFound:
+
+                    statusCode = HttpStatusCode.NotFound;
+
+                    problem = new CustomProblemDetails
+                    {
+                        Title = notFound.Message,
+                        Status = (int)statusCode,
+                        Type = nameof(EntityNotFoundException),
+                        Detail = notFound.InnerException?.Message,
+                    };
+                    break;
+
+                default:
+
+                    problem = new CustomProblemDetails
+                    {
+                        Title = ex.Message,
+                        Status = (int)statusCode,
+                        Type = nameof(HttpStatusCode.InternalServerError),
+                        Detail = ex.StackTrace,
+                    };
+                    break;
+            }
+
+            httpContext.Response.StatusCode = (int)statusCode;
+            await httpContext.Response.WriteAsJsonAsync(problem);
+        }
+    }
+}
--- HRLeaveManagement.Api/Program.cs
+++ HRLeaveManagement.Api/Program.cs
@@ -1,3 +1,4 @@
+using HRLeaveManagement.Api.Middlewares;
 using HRLeaveManagement.Application;
 using HRLeaveManagement.Infrastructure;
 using HRLeaveManagement.Persistence;
@@ -22,6 +23,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
